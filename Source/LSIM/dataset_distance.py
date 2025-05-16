6dc6d76d3c57456705429aa94bf357f1c332edac
beneitez <mikibene@gmail.com>
2025-05-16 11:33:24 +0200

    refactor: Improve dataset loading and transforms

--- Source/LSIM/dataset_distance.py
+++ Source/LSIM/dataset_distance.py
@@ -85,7 +85,6 @@ class DatasetDistance(Dataset):
         """
         directory = self.dataPaths[idx]
         fileNames = os.listdir(directory)
-        print(directory,fileNames) # Print directory and files for debugging
         fileNames.sort() # Ensure consistent file processing order
 
         listFrames = [] # Stores loaded image frames
@@ -127,10 +126,15 @@ class DatasetDistance(Dataset):
             if frame.shape[2] == 4:
                 frame = frame[...,:-1]
 
+
             # Extract distance from filename (assuming format like 'XX.fileType')
             # This part assumes the distance is a two-digit number before the extension
-            start = len(fileName) - 6
-            end = len(fileName) - 4
+            if self.fileType == "png" or self.fileType == "npz":
+                start = len(fileName) - 6
+                end = len(fileName) - 4
+            elif self.fileType == "h5":
+                start = len(fileName) - 5
+                end = len(fileName) - 3
             listDist.append( float(fileName[start:end]) )
             listFrames.append(frame)
 
@@ -147,6 +151,7 @@ class DatasetDistance(Dataset):
 
 
         frames = np.array(listFrames)
+        print("Number of frames: ", frames.shape)
         # Optional check for expected number of frames (commented out)
         #if frames.shape[0] != 11:
         #    print("%s is missing files!" % directory)
@@ -310,7 +315,7 @@ class TransformsTrain(object):
 
             # Random channel swap (for 3-channel images)
             if ref.shape[2] == 3: # Only swap if it's an RGB-like image
-                channelOrder = [0,1,2]
+                channelOrder = [0,1,2,3,4,5,6]
                 random.shuffle(channelOrder)
                 ref = ref[..., channelOrder]
                 oth = oth[..., channelOrder]
@@ -335,8 +340,10 @@ class TransformsTrain(object):
 
         # Normalization: Scale pixel values to [normMin, normMax] based on min/max in the batch
         # Calculate min and max across all images and spatial dimensions in the batch
+        print(f"Applying TransformsTrain normalization for sample from {path}")
         dMin = np.minimum( np.min(resultRef, axis=(0,1,2)), np.min(resultOther, axis=(0,1,2)) )
         dMax = np.maximum( np.max(resultRef, axis=(0,1,2)), np.max(resultOther, axis=(0,1,2)) )
+
 
         # Apply normalization per channel
         # Handle cases where min == max for a channel
@@ -354,6 +361,7 @@ class TransformsTrain(object):
         else: # No channels have constant value
             resultRef = (self.normMax - self.normMin) * ( (resultRef - dMin) / (dMax - dMin) ) + self.normMin
             resultOther = (self.normMax - self.normMin) * ( (resultOther - dMin) / (dMax - dMin) ) + self.normMin
+
 
         # Convert numpy arrays to torch.Tensors and change channel order (HWC to CHW)
         resultRef = torch.from_numpy(resultRef.transpose(0,3,1,2)).float()
@@ -579,148 +587,3 @@ class TransformsInference(object):
         self.mean, self.std = [mean, std]
 
         return mean, std
-
-# ------------------------------------------------- 
-# TRANSFORMS TO APPLY TO THE DATA
-# -------------------------------------------------
-
-# combines randomFlip, randomRotation90, randomCrop,
-# channelSwap, toTensor and normalization for efficiency
-class TransformsTrain(object):
-    def __init__(self, outputSize, normMin=0, normMax=255):
-        self.outputSize = outputSize
-        self.normMin = normMin
-        self.normMax = normMax
-        self.angles = [0,1,2,3]
-
-    def __call__(self, sample):
-        dist = sample["distance"]
-        reference = sample["reference"]
-        other = sample["other"]
-        path = sample["path"]
-
-        resultRef = np.zeros([reference.shape[0], self.outputSize, self.outputSize, reference.shape[3]])
-        resultOther = np.zeros([other.shape[0], self.outputSize, self.outputSize, other.shape[3]])
-
-        for i in range(reference.shape[0]):
-            ref = reference[i]
-            oth = other[i]
-
-            # flip
-            rand = random.randint(0, 3)
-            if rand == 1:
-                ref = np.fliplr(ref)
-                oth = np.fliplr(oth)
-            if rand == 2:
-                ref = np.flipud(ref)
-                oth = np.flipud(oth)
-            if rand == 3:
-                ref = np.flipud( np.fliplr(ref) )
-                oth = np.flipud( np.fliplr(oth) )
-
-            # rot90
-            angle = random.choice(self.angles)
-            ref = np.rot90(ref, angle)
-            oth = np.rot90(oth, angle)
-
-            # channel swap
-            channelOrder = [0,1,2]
-            random.shuffle(channelOrder)
-            ref = ref[..., channelOrder]
-            oth = oth[..., channelOrder]
-
-            # crop
-            if ref.shape[0] <= self.outputSize or ref.shape[1] <= self.outputSize:
-                resultRef[i] = ref
-                resultOther[i] = oth
-                continue
-
-            top = np.random.randint(0, ref.shape[0] - self.outputSize)
-            left = np.random.randint(0, ref.shape[1] - self.outputSize)
-
-            resultRef[i] = ref[top : top+self.outputSize,  left : left+self.outputSize]
-            resultOther[i] = oth[top : top+self.outputSize,  left : left+self.outputSize]
-
-        # normalization
-        dMin = np.minimum( np.min(resultRef, axis=(0,1,2)), np.min(resultOther, axis=(0,1,2)) )
-        dMax = np.maximum( np.max(resultRef, axis=(0,1,2)), np.max(resultOther, axis=(0,1,2)) )
-        if (dMin == dMax).all():
-            resultRef = resultRef - dMin
-            resultOther = resultOther - dMin
-        elif (dMin == dMax).any():
-            for i in range(dMin.shape[0]):
-                if dMin[i] == dMax[i]:
-                    resultRef[..., i] = resultRef[..., i] - dMin[i]
-                    resultOther[..., i] = resultOther[..., i] - dMin[i]
-                else:
-                    resultRef[..., i] = (self.normMax - self.normMin) * ( (resultRef[..., i] - dMin[i]) / (dMax[i] - dMin[i]) ) + self.normMin
-                    resultOther[..., i] = (self.normMax - self.normMin) * ( (resultOther[..., i] - dMin[i]) / (dMax[i] - dMin[i]) ) + self.normMin
-        else:
-            resultRef = (self.normMax - self.normMin) * ( (resultRef - dMin) / (dMax - dMin) ) + self.normMin
-            resultOther = (self.normMax - self.normMin) * ( (resultOther - dMin) / (dMax - dMin) ) + self.normMin
-
-        # toTensor
-        resultRef = torch.from_numpy(resultRef.transpose(0,3,1,2)).float()
-        resultOther = torch.from_numpy(resultOther.transpose(0,3,1,2)).float()
-        dist = torch.from_numpy(np.array(dist)).float()
-
-        return {"reference": resultRef, "other": resultOther, "distance": dist, "path": path}
-
-
-# combines resize, toTensor and normalization for efficiency
-class TransformsInference(object):
-    def __init__(self, outputSize, order, normMin = 0, normMax = 255):
-        self.normMin = normMin
-        self.normMax = normMax
-        self.outputSize = outputSize
-        self.order = order
-
-    def __call__(self, sample):
-        dist = sample["distance"]
-        reference = sample["reference"]
-        other = sample["other"]
-        path = sample["path"]
-
-        # repeat for scalar fields
-        if reference.shape[reference.ndim-1] == 1:
-            reference = np.repeat(reference, 3, axis=reference.ndim-1)
-        if other.shape[other.ndim-1] == 1:
-            other = np.repeat(other, 3, axis=other.ndim-1)
-
-        # resize
-        if self.outputSize and (self.outputSize != reference.shape[1] or self.outputSize != reference.shape[2]):
-            resultRef = np.zeros([reference.shape[0], self.outputSize, self.outputSize, reference.shape[3]])
-            resultOther = np.zeros([other.shape[0], self.outputSize, self.outputSize, other.shape[3]])
-
-            zoom1 = [1, self.outputSize / reference.shape[1], self.outputSize / reference.shape[2], 1]
-            resultRef = scipy.ndimage.zoom(reference, zoom1, order=self.order)
-            zoom2 = [1, self.outputSize / other.shape[1], self.outputSize / other.shape[2], 1]
-            resultOther = scipy.ndimage.zoom(other, zoom2, order=self.order) 
-        else:
-            resultRef = reference
-            resultOther = other
-
-        # normalization
-        dMin = np.minimum( np.min(resultRef, axis=(0,1,2)), np.min(resultOther, axis=(0,1,2)) )
-        dMax = np.maximum( np.max(resultRef, axis=(0,1,2)), np.max(resultOther, axis=(0,1,2)) )
-        if (dMin == dMax).all():
-            resultRef = resultRef - dMin
-            resultOther = resultOther - dMin
-        elif (dMin == dMax).any():
-            for i in range(dMin.shape[0]):
-                if dMin[i] == dMax[i]:
-                    resultRef[..., i] = resultRef[..., i] - dMin[i]
-                    resultOther[..., i] = resultOther[..., i] - dMin[i]
-                else:
-                    resultRef[..., i] = (self.normMax - self.normMin) * ( (resultRef[..., i] - dMin[i]) / (dMax[i] - dMin[i]) ) + self.normMin
-                    resultOther[..., i] = (self.normMax - self.normMin) * ( (resultOther[..., i] - dMin[i]) / (dMax[i] - dMin[i]) ) + self.normMin
-        else:
-            resultRef = (self.normMax - self.normMin) * ( (resultRef - dMin) / (dMax - dMin) ) + self.normMin
-            resultOther = (self.normMax - self.normMin) * ( (resultOther - dMin) / (dMax - dMin) ) + self.normMin
-
-        # toTensor
-        resultRef = torch.from_numpy(resultRef.transpose(0,3,1,2)).float()
-        resultOther = torch.from_numpy(resultOther.transpose(0,3,1,2)).float()
-        dist = torch.from_numpy(np.array(dist)).float()
-
-        return {"reference": resultRef, "other": resultOther, "distance": dist, "path": path}
